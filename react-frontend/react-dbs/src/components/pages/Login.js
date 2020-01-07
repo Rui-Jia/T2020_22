e7f2f447b7522786439529496d568da388cf194d
Low Rui Jia <lowruijia96@gmail.com>
2020-01-07 13:07:57 +0800

    Login include backend

--- react-frontend/react-dbs/src/components/pages/Login.js
+++ react-frontend/react-dbs/src/components/pages/Login.js
@@ -1,52 +1,78 @@
 import React, { Component } from 'react';
 import header from '../assets/header.jpg';
+import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 
 export class Login extends Component {
-   
+
     constructor(props) {
         super(props);
         this.state = {
-            username : '',
+            username: '',
             password: '',
-            value: ''
+            value: '',
+            customerid: ''
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleChange(event) 
-      {
-        this.setState({
-            [event.target.name]:event.target.value
-        })
-      }
+    }
 
-      handleSubmit(event) {
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
         // alert('A name was submitted: ' + this.state.value);
-        this.props.UserLogin(this.state.username, this.state.password);
-      }
+        // this.props.UserLogin(this.state.username, this.state.password);
+        event.preventDefault();
+        var data = {};
+        data.username = this.state.username;
+        data.password = this.state.password;
+        axios.post('http://127.0.0.1:8080/login', data)
+            .then((response) => {
+                console.log(response);
+                if (response.status > 300) {
+                    alert('fail')
+                } else {
+                    // alert('success')
+                    console.log('success')
+                    // console.log(response)
+                    // this.setState({customerid: response.data.customerid})
+                    window.sessionStorage.setItem("customerid", response.data.customerId);
+                    // console.log(response.data);
+                    // console.log(response.data.customerId);
+                    this.props.history.push('/Overview/');
+                }
+            }, (error) => {
+                console.log(error);
+            });
+    }
 
     render() {
+
+
         return (
-            <div class="login">
-                <div class="header">
+            <div className="login">
+                <div className="header">
                     <img src={header} alt="Logo" />
                 </div>
 
                 <div className='form-wrapper'>
                     <h2>LOGIN</h2>
-                    <form onSubmit={this.handleSubmit}>
+                    <form>
                         <label>
                             Username:
-                        <input value={this.state.username} onChange={this.handleChange} type="text" placeholder="Username" name="username"/>
+                        <input value={this.state.username} onChange={this.handleChange} type="text" placeholder="Username" name="username" />
                         </label><br />
 
                         <label>
                             Password:
-                        <input type="password" value={this.state.password} onChange={this.handleChange} name="password"/>
+                        <input type="password" value={this.state.password} onChange={this.handleChange} name="password" />
                         </label><br /><br />
 
-                        <input type="submit" value="Login" />
+                        <button onClick={this.handleSubmit}>submit</button>
                     </form>
                 </div>
             </div>
@@ -58,10 +84,10 @@ export default Login
 
 // export function UserLogin(username, password) {
 //     return async dispatch => {
-//       dispatch(UserLoginPending());
 //       try {
-//         // let response = await fetch('http://127.0.0.1:5000/login', {
-//         let response = await fetch(backendEndpoint + '/login', {
+//           alert('enter function')
+//         // let response = await fetch('http://127.0.0.1:8080/login', {
+//         let response = await fetch('http://127.0.0.1:8080/login', {
 //           method: 'POST',
 //           headers: {
 //             Accept: 'application/json',
@@ -74,13 +100,12 @@ export default Login
 //         })
 //         let responseJson = await response.json();
 //         if (response.status > 300) {
-//           dispatch(UserLoginFailure(responseJson.error));
+//           alert('fail')
 //         } else {
-//           dispatch(UserLoginSuccess(responseJson.metadata));
-//           nav.push('Overview');
+//             alert('success')
 //         }
 //       } catch (error) {
-//         Alert.alert(error)
+//         alert('error')
 //       }
 //     };
 //   }
