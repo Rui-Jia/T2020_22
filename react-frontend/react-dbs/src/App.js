2e439b42d7fb68c91e222c562fa7ea0c6edc5dd2
Andrew Woon Yong Tian <1800984@sit.singaporetech.edu.sg>
2020-01-07 11:07:28 +0800

    Set-up for Overview

--- react-frontend/react-dbs/src/App.js
+++ react-frontend/react-dbs/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Overview from './components/pages/Overview'
+import Header from './components/layout/Header'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+        <Route exact path="/" render={props => (
+          <React.Fragment>
+          </React.Fragment>)} />
+        <Route path="/Overview" component={Overview} />
+      </div>
     </div>
+  </Router>
   );
 }
 
