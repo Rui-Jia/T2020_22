5e3d9e423f7ea50f7580481cabd7ed5de26eb70c
Low Rui Jia <lowruijia96@gmail.com>
2020-01-07 16:11:06 +0800

    backend marketingservice

---
+++ backend/techtrek/src/main/java/com/group22/techtrek/controller/IndexController.java
@@ -0,0 +1,32 @@
+package com.group22.techtrek.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.group22.techtrek.model.Marketing;
+import com.group22.techtrek.model.User;
+import com.group22.techtrek.service.MarketingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class IndexController {
+    
+    @Autowired
+    MarketingService userService;
+    
+    @RequestMapping(value="/marketing", method=RequestMethod.GET)
+    public List<Marketing> getMarketing(@RequestBody User user) {
+        System.out.println("Inside get marketing");
+        List<Marketing> marketingList = new ArrayList<>();
+
+        return marketingList;
+    }
+    
+}
---
+++ backend/techtrek/src/main/java/com/group22/techtrek/service/MarketingService.java
@@ -0,0 +1,8 @@
+package com.group22.techtrek.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarketingService {
+    
+}
